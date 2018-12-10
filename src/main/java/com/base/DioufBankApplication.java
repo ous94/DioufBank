11b668238b63e6f6c4eff33abe51fbf01efadc73
barry <ousmanebarry994@gmail.com>
2018-12-10 11:42:32 +0000

    firt commit

---
+++ src/main/java/com/base/DioufBankApplication.java
@@ -0,0 +1,12 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioufBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioufBankApplication.class, args);
+	}
+}
